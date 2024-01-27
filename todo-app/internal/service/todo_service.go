6d1bf01ef95446a1fc3e9e3fb51e92a7b76c83d2
AdamKaczmarski <adam.kacz10@gmail.com>
2024-01-27 13:29:21 +0000

    tests, swapped to map instead of slice

--- todo-app/internal/service/todo_service.go
+++ todo-app/internal/service/todo_service.go
@@ -1,52 +1,71 @@
 package service
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Service interface {
-	GetTodos() []todo
-	AddTodo(note *string) int
-	MarkTodoAsDone(id int)
-    RemoveTodo(id int)
+	GetTodos() map[uint]todo
+	AddTodo(note *string) uint
+	MarkTodoAsDone(id uint)
+	RemoveTodo(id uint)
 }
 
 type todo struct {
-	id   int
 	done bool
 	note *string
 }
 
 type service struct {
-	todos   *[]todo
-	last_id *int
+	mu      *sync.Mutex
+	todos   map[uint]todo
+	last_id *uint
 }
 
 func NewService() *service {
-	todos := make([]todo, 10)
-	last_id := 1
-	return &service{&todos, &last_id}
+	todos := make(map[uint]todo)
+	var last_id uint = 1
+	return &service{&sync.Mutex{}, todos, &last_id}
 }
 
-func (s *service) GetTodos() []todo {
-	var cloneTodos []todo = make([]todo, len(*s.todos))
-	copy(cloneTodos, *s.todos)
-	return cloneTodos
+func (s *service) GetTodos() map[uint]todo {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	clonedMap := make(map[uint]todo, len(s.todos))
+	for k, v := range s.todos {
+		clonedMap[k] = v
+	}
+	return clonedMap
 }
 
-func (s *service) AddTodo(note *string) int {
+func (s *service) AddTodo(note *string) uint {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	id := *s.last_id
-	id += 1
+	*s.last_id += 1
 	newTodo := todo{
-		id:   id,
 		done: false,
 		note: note,
 	}
-	tmp := append(*s.todos, newTodo)
-	s.todos = &tmp
-
+	s.todos[id] = newTodo
 	return id
 }
-func (s *service) MarkTodoAsDone(id int) {
-	panic("todo")
+func (s *service) MarkTodoAsDone(id uint) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if todo, ok := s.todos[id]; ok {
+		todo.done = true
+		s.todos[id] = todo
+	}
 }
 
-func (s *service) RemoveTodo(id int) {
+func (s *service) RemoveTodo(id uint) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.todos, id)
+}
 
+func (t todo) String() string {
+	return fmt.Sprintf("Todo{note: \"%s\", done: %v}", *t.note, t.done)
 }
--- todo-app/internal/service/todo_service_test.go
+++ todo-app/internal/service/todo_service_test.go
@@ -1,10 +1,56 @@
 package service
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestTodoService(t *testing.T) {
-    t.Run("IncrementingLastId",func(t *testing.T) {})
-    t.Run("AddingTodos",func(t *testing.T) {})
-    t.Run("MarkingTodoAsDone",func(t *testing.T) {})
-    t.Run("RemovingTodo",func(t *testing.T) {})
+	note := "note"
+	t.Run("IncrementingLastId", func(t *testing.T) {
+		s := NewService()
+		expected := 1
+		for i := 0; i < 6; i++ {
+			_ = s.AddTodo(&note)
+			expected += 1
+		}
+		if expected != int(*s.last_id) {
+			panic(fmt.Sprintf("expected: %d, actual: %d", expected, *s.last_id))
+		}
+	})
+	t.Run("AddingTodos", func(t *testing.T) {
+		s := NewService()
+		expected := make(map[uint]todo)
+		for i := 1; i < 6; i++ {
+			expected[uint(i)] = todo{false, &note}
+			_ = s.AddTodo(&note)
+		}
+		if !reflect.DeepEqual(expected, s.todos) {
+			panic(fmt.Sprintf("notes, not equal, expected: %+v, actual %+v", expected, s.todos))
+		}
+	})
+	t.Run("MarkingTodoAsDone", func(t *testing.T) {
+		s := NewService()
+		str := "doneNote"
+		id := s.AddTodo(&str)
+		s.MarkTodoAsDone(id)
+		for _, todo := range s.todos {
+			if !todo.done {
+				panic(fmt.Sprintf("Todo should be done, actual %v", todo))
+			}
+		}
+	})
+	t.Run("RemovingTodo", func(t *testing.T) {
+		s := NewService()
+		str := "toBeRemoved"
+		id := s.AddTodo(&str)
+		if len(s.todos) != 1 {
+			panic("Todo was not added")
+		}
+		s.RemoveTodo(id)
+		if len(s.todos) != 0 {
+			panic(fmt.Sprintf("Todo was not removed %v", s.todos))
+		}
+	})
 }
